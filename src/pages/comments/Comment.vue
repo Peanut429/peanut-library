41cf57a88101b89faef0d6b9496690ea1a2eb6c5
fuhui <18915000292@163.com>
2018-08-31 00:39:55 +0800

    项目初始化，添加服务端文件

---
+++ src/pages/comments/Comment.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        评论过的书籍
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
+
---
+++ src/pages/comments/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Comment from './Comment'
+
+Vue.config.productionTip = false
+
+const app = new Vue(Comment)
+app.$mount()
